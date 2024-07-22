31af7e3ee868c7abfb331c82f86086a2e11d43e9
SARANGATHALAGALAGE <140165039+SARANGATHALAGALAGE@users.noreply.github.com>
2024-07-22 23:40:27 +0530

    Create q02.scala

---
+++ q02.scala
@@ -0,0 +1,8 @@
+object temperature {
+  def main(args: Array[String]): Unit = {
+    val temp: Double = 35
+    val fahren: Double = (temp * 1.8000) + 32.00
+
+    print("Fahrenhite = " + fahren)
+  }
+}
